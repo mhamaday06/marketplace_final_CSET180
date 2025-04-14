d77082167418b86e7b7adf54cf7c9f16dcbf8355
mattthew-ui <MatthewBerryhill22@outlook.com>
2025-04-14 10:37:25 -0400

    attempting to merge

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request, jsonify, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text, create_engine, text
-
-conn_str = "mysql://root:cset155@localhost/exam_management_2"
+from enum import IntEnum
+import random
+import bcrypt
+import mysql.connector
+conn_str = "mysql://root:cset155@localhost/ecommerce_application"
 engine = create_engine(conn_str, echo = True)
 conn = engine.connect()
 
 app = Flask(__name__)
 
+
 # Setup MySQL connection for Flask-SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:cset155@localhost/exam_management_2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:cset155@localhost/ecommerce_application"
+app.config['SECRET_KEY'] = 'mysecretkey123'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
